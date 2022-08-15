3e63f4349ed93304022e645096b0f90b0f74e884
santinaranjo <marketingsanti@gmail.com>
2022-08-15 19:04:29 +0200

    Config Recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { RecoilRoot } from "recoil";
 
 const rootElement = document.getElementById("app")
 
 const root = ReactDOM.createRoot(rootElement!);
 
 root.render(
+    <RecoilRoot>
         <App />
+    </RecoilRoot>
 );
