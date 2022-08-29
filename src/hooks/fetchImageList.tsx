97f36b0502bf5caf07101185261750c6ebd9ebf8
santinaranjo <marketingsanti@gmail.com>
2022-08-30 01:46:43 +0200

    Sustituido workaround usando setTimeout por flushSync en fetchImageList

--- src/hooks/fetchImageList.tsx
+++ src/hooks/fetchImageList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { flushSync } from 'react-dom'
 import { useLocalStorage } from './useLocalStorage'
 
 export const fetchImageList = (
@@ -7,7 +8,7 @@ export const fetchImageList = (
     localStorageKey: string
 ) => {
     const [data, setData] = useLocalStorage(localStorageKey, [])
-    const [loading, setLoading] = React.useState<Boolean>(true)
+    const [loading, setLoading] = React.useState<any>(true)
 
     const fetchBlobList = async (list: Array<string>) => {
         const res = await Promise.all(list.map((u) => fetch(`${apiURL}${u}`)))
@@ -18,18 +19,14 @@ export const fetchImageList = (
         const imageRes = await Promise.all(sprites.map((r) => fetch(r)))
         const imageBlobs = await Promise.all(imageRes.map((r) => r.blob()))
         return imageBlobs
-        // const imageURLs = await Promise.all(
-        //     imageBlobs.map((url) => URL.createObjectURL(url))
-        // )
-        // return imageURLs
     }
 
     React.useEffect(() => {
-        fetchBlobList(listOfURLs).then(async (res) => {
-            setData(res)
-            setTimeout(() => {
-                setLoading(false)
-            }, 1000)
+        fetchBlobList(listOfURLs).then((res) => {
+            flushSync(() => {
+                setData(res)
+            })
+            setLoading(false)
         })
     }, [])
 
