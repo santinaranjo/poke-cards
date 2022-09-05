045b17db7a3e127e6a586591794485df52045007
santinaranjo <marketingsanti@gmail.com>
2022-09-05 23:43:21 +0200

    Tiempo en finalizar añadido a Endgame

--- src/components/Endgame/Endgame.tsx
+++ src/components/Endgame/Endgame.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { endgameCardContext } from '../../context/endgameContext'
 import { Controlls } from '../Controlls/Controlls'
 import { Section } from './styles'
+import { stopWatchTimeContext } from '../../context/stopWatchContext'
 
 export const Endgame = () => {
     const [endgameCard, setEndgameCard] = useRecoilState(endgameCardContext)
+    const stopWatchTime = useRecoilValue(stopWatchTimeContext)
+
     return (
         <React.Fragment>
             {endgameCard ? (
                 <React.Fragment>
                     <Section>
                         <div>
+                            <p>{`Has tardado ${stopWatchTime.min}:${stopWatchTime.seg} mins`}</p>
                             <Controlls controllSelector="restart" />
                             <Link to="/gamemode">Volver al Menu</Link>
                         </div>
